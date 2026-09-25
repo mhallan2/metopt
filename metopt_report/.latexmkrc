a3be77a6c99e3df3e997321f182843bb494548aa
mhallan2 <vishnyakovgg@gmail.com>
2026-09-25 10:31:34 +0300

    Report is done mostly.

---
+++ metopt_report/.latexmkrc
@@ -0,0 +1,3 @@
+$pdf_mode = 4;
+$aux_dir = 'build';
+$lualatex = 'lualatex -shell-escape -interaction=nonstopmode -synctex=1 %O %S';
---
+++ metopt_report/code/methods.py
@@ -0,0 +1,8 @@
+def parabola(function, interval, eps):
+    # Реализация метода парабол
+    pass
+
+
+def broken_lines(function, interval, eps):
+    # Метод ломаных с адаптивной оценкой M
+    pass
